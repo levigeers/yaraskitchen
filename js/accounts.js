3138b7a2334ffa18a3920555963d08de38d06e5b
levigeers2000 <l.geers@flavour.nl>
2021-12-30 00:44:03 +0100

    attempt on logging in

--- js/accounts.js
+++ js/accounts.js
@@ -1,35 +1,27 @@
-const loginButton = document.getElementById("loginButton");
-const registerButton = document.getElementById("registerButton");
+var loginButton = document.getElementById("loginButton");
+var registerButton = document.getElementById("registerButton");
 
-loginButton.onclick = login;
-registerButton.onclick = register;
+loginButton.onclick = onLoginButton;
+registerButton.onclick = onRegisterButton;
 
-function login() {
-    // $.ajax({
-    //     url: "http://127.0.0.1:5500/php/accounts.php",
-    //     type: "get",
-    //     data: {action: 'loginUser', 'username':'Levi'},
-    //     success: function(data) {
-    //         console.log("hello world");
-    //     }
-    // })
+function onLoginButton() {
+    $.ajax({
+        url: "http://127.0.0.1:5500/php/accounts.php",
+        type: "get",
+        data: {action: 'loginUser', username: 'Levi'},
+        success: function(data) {
+            console.log(data);
+        }
+    })
 }
 
-function register() {
-    // var mysql = require('mysql');
-
-    // var con = mysql.createConnection({
-    //     host: "localhost",
-    //     user: "root",
-    //     password: "",
-    //     database: "yarabakery"
-    // });
-
-    // con.connect(function(err) {
-    // if (err) throw err;
-    //     con.query("SELECT * FROM user", function (err, result, fields) {
-    //         if (err) throw err;
-    //         console.log(result);
-    //     });
-    // });
+function onRegisterButton() {
+    $.ajax({
+        url: "http://127.0.0.1:5500/php/accounts.php",
+        type: "get",
+        data: {action: 'registerUser', username: 'Levi'},
+        success: function(data) {
+            console.log(data);
+        }
+    })
 }
